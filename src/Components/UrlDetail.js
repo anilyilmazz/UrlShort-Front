19ca86b1355807b1b340d1c98d8238761608a7b3
Deniz Can DİNÇ <deniz@agencylook.com>
2021-01-31 18:30:43 +0300

    ui fixed

--- src/Components/UrlDetail.js
+++ src/Components/UrlDetail.js
@@ -1,18 +1,37 @@
 import React from 'react'
-import {Link} from 'react-router-dom';
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { Link } from 'react-router-dom';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 function UrlDetail({ urlData }) {
     console.log(window.location);
+   
+     function alertFunction(e) 
+     {
+        e.preventDefault();
+        alert("Copied !");
+    }
     return (
         <div className="card w-75 mx-auto">
             <div className="card-body">
-            <p style={{float: "left"}}>{urlData.url}</p>
-            <Link to={{pathname: `/${urlData.shorturl}`}} style={{float: "right"}}>
-            <p style={{float: "right"}}>{`${window.location.host}/${urlData.shorturl}`}</p></Link>
-            <CopyToClipboard text={window.location.host + '/' + urlData.shorturl} >
-                <button style={{float: "right"}}>Copy</button>
-            </CopyToClipboard>
+                <div className="container">
+                    <div className="row">
+                        <span>Original Url : </span> <br/>  
+                           <p>{urlData.url}</p>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-3">
+                            <span>Short Url : </span> <br/>
+                            <Link to={{ pathname: `/${urlData.shorturl}` }}>
+                                <p >{`${window.location.host}/${urlData.shorturl}`}</p></Link>
+                        </div>
+                        <div className="col-md-3">
+                            <CopyToClipboard text={window.location.host + '/' + urlData.shorturl} >
+                                <button onClick={alertFunction} class="btn btn-outline-primary">Copy Short Url</button>
+                            </CopyToClipboard>
+                        </div>
+
+                    </div>
+                </div>
             </div>
         </div>
     )
